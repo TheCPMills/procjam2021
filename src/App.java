2cb697dbfd9c9ac74a6b04472bcd5fc95fe9c036
Ryan Stebe <rjstebe1@gmail.com>
2021-11-12 12:44:05 -0500

    refactored generate() parameters

--- src/App.java
+++ src/App.java
@@ -1,8 +1,6 @@
-import javanoise.noise.*;
-
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        TerrainGenerator.generate(new Simplex(1234567890), 1024, 128, 15, 7);
+        TerrainGenerator.generate(2034912047, 1024, 128, 15);
     }
 }

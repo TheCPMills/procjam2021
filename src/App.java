993e3f254e2e5dc9fc6de2200452e85f37f444c4
Chase <cpmills26@gmail.com>
2021-11-15 17:00:04 -0500

    Milestone 1

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         int seed = 1234567890;
-        TerrainGenerator.generate(seed, 1024, 1024, 15);
+        TerrainGenerator.generate(seed, 2048, 1024, 2.5);
     }
 }
